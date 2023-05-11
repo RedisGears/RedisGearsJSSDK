6ff3cda90ebbdbcf0461a564ec7d5ea4e0570e09
Leibale <me@leibale.com>
2023-05-11 12:08:36 +0300

    add INCR

--- packages/sdk/lib/commands.ts
+++ packages/sdk/lib/commands.ts
@@ -1,6 +1,7 @@
 import GET from '@redis/client/dist/lib/commands/GET.js';
 import SET from '@redis/client/dist/lib/commands/SET.js';
 import HSET from '@redis/client/dist/lib/commands/HSET.js';
+import INCR from '@redis/client/dist/lib/commands/INCR.js';
 
 export default {
   GET,
@@ -8,5 +9,7 @@ export default {
   SET,
   set: SET,
   HSET,
-  hSet: HSET
+  hSet: HSET,
+  INCR,
+  incr: INCR
 } as const;
